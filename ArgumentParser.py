c1c73bbd434cacefc0a236f685a05028fe89005b
bard <carruthers.n.t@gmail.com>
2020-09-14 21:23:44 -0300

    added type checking to path argument

--- ArgumentParser.py
+++ ArgumentParser.py
@@ -7,8 +7,15 @@ class ArgumentParser():
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Checks all images for width and ratio and filters them into appropriate sub-directories.')
 
+    def _path_type(self, path):
+        if not os.path.isdir(path):
+            msg = '%s not a valid directory' % path
+            raise argparse.ArgumentTypeError(msg)
+        else:
+            return path
+
     def add_arguments(self):
-        self.parser.add_argument('path', default=os.getcwd(), nargs='?', help='directory to be used by %(prog)s. Default is current working directory.')
+        self.parser.add_argument('path', type=self._path_type, default=os.getcwd(), nargs='?', help='directory to be used by %(prog)s. Default is current working directory.')
         self.parser.add_argument('-v', '--verbose', action='store_true', help='increase verbosity')
 
     def get_args(self):
